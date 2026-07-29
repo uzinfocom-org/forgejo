ca536805b27ecfb66227fede32b378403a545b8b
xfeusw <khamrakulovkamron@gmail.com>
2026-07-29 17:27:55 +0500

    reformat

--- src/Forgejo/API/Organization.hs
+++ src/Forgejo/API/Organization.hs
@@ -1,21 +1,21 @@
 module Forgejo.API.Organization
-	  ( OrgRoutes (..)
-	  ) where
+  ( OrgRoutes (..)
+  ) where
 
-	import Data.Text (Text)
-	import Forgejo.Types.CreateRepositoryOption (CreateRepositoryOption)
-	import Forgejo.Types.Repository (Repository)
-	import GHC.Generics (Generic)
-	import Servant (Capture, JSON, Post, ReqBody, type (:-), type (:>))
+import Data.Text (Text)
+import Forgejo.Types.CreateRepositoryOption (CreateRepositoryOption)
+import Forgejo.Types.Repository (Repository)
+import GHC.Generics (Generic)
+import Servant (Capture, JSON, Post, ReqBody, type (:-), type (:>))
 
-	data OrgRoutes route = OrgRoutes
-	  { createOrgRepositoryApi
-	      :: route
-	        :- "org"
-	          :> Capture "org" Text
-						:> "repos"
-	          :> ReqBody '[JSON] CreateRepositoryOption
-	          :> Post '[JSON] [Repository]
-	  -- ^ POST /orgs/{org}/repos
-	  }
-	  deriving (Generic)
+data OrgRoutes route = OrgRoutes
+  { createOrgRepositoryApi
+      :: route
+        :- "org"
+          :> Capture "org" Text
+          :> "repos"
+          :> ReqBody '[JSON] CreateRepositoryOption
+          :> Post '[JSON] [Repository]
+  -- ^ POST /orgs/{org}/repos
+  }
+  deriving (Generic)
