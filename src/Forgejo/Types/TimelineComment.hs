b44541750f9d73e803ff293d64c2b9881d59ffa7
xfeusw <khamrakulovkamron@gmail.com>
2026-08-04 14:32:11 +0500

    refactor: label fix

--- src/Forgejo/Types/TimelineComment.hs
+++ src/Forgejo/Types/TimelineComment.hs
@@ -4,7 +4,6 @@
 
 module Forgejo.Types.TimelineComment
   ( TimelineComment (..)
-  , TimelineCommentPayload (..)
   ) where
 
 import Data.Aeson (FromJSON (..), ToJSON (..), genericToJSON, withObject, (.:))
@@ -19,40 +18,34 @@ import Forgejo.Types.TrackedTime (TrackedTime)
 import Forgejo.Types.User (User)
 import GHC.Generics (Generic)
 
-arpOptions :: Options
-arpOptions = defaultOptions{fieldLabelModifier = camelTo2 '_' . drop 3}
-
-runOptions :: Options
-runOptions = defaultOptions{fieldLabelModifier = camelTo2 '_' . drop 3}
-
 data TimelineComment = TimelineComment
-  { assignee :: User
-  , assigneeTeam :: Team
-  , body :: Text
-  , createdAt :: UTCTime
-  , htmlUrl :: Text
-  , id :: Int
-  , issueUrl :: Text
-  , label :: Label
-  , milestone :: Milestone
-  , newRef :: Text
-  , newTitle :: Text
-  , oldMilestone :: Milestone
-  , oldProjectId :: Int
-  , oldRef :: Text
-  , oldTitle :: Text
-  , projectId :: Int
-  , pullRequestUrl :: Text
-  , refAction :: Text
-  , refComment :: Comment
-  , refCommitSha :: Text
-  , removedAssignee :: Bool
-  , resolveDoer :: User
-  , reviewId :: Int
-  , trackedTime :: TrackedTime
+  { tcAssignee :: User
+  , tcAssigneeTeam :: Team
+  , tcBody :: Text
+  , tcCreatedAt :: UTCTime
+  , tcHtmlUrl :: Text
+  , tcId :: Int
+  , tcIssueUrl :: Text
+  , tcLabel :: Label
+  , tcMilestone :: Milestone
+  , tcNewRef :: Text
+  , tcNewTitle :: Text
+  , tcOldMilestone :: Milestone
+  , tcOldProjectId :: Int
+  , tcOldRef :: Text
+  , tcOldTitle :: Text
+  , tcProjectId :: Int
+  , tcPullRequestUrl :: Text
+  , tcRefAction :: Text
+  , tcRefComment :: Comment
+  , tcRefCommitSha :: Text
+  , tcRemovedAssignee :: Bool
+  , tcResolveDoer :: User
+  , tcVeviewId :: Int
+  , tcTrackedTime :: TrackedTime
   , tcType :: Text
-  , updatedAt :: UTCTime
-  , user :: User
+  , tcUpdatedAt :: UTCTime
+  , tcUser :: User
   }
   deriving stock (Eq, Generic, Show)
 
@@ -87,23 +80,3 @@ instance FromJSON TimelineComment where
       <*> o .: "type"
       <*> o .: "updated_at"
       <*> o .: "user"
-
-instance ToJSON TimelineComment where
-  toJSON = genericToJSON runOptions
-
-data TimelineCommentPayload = TimelineCommentPayload
-  { arpAction :: Text
-  , arpRun :: TimelineComment
-  , arpPriorStatus :: Text
-  }
-  deriving stock (Eq, Generic, Show)
-
-instance FromJSON TimelineCommentPayload where
-  parseJSON = withObject "TimelineCommentPayload" $ \o ->
-    TimelineCommentPayload
-      <$> o .: "action"
-      <*> o .: "run"
-      <*> o .: "prior_status"
-
-instance ToJSON TimelineCommentPayload where
-  toJSON = genericToJSON arpOptions
--- src/Forgejo/Types/TrackedTime.hs
+++ src/Forgejo/Types/TrackedTime.hs
@@ -14,12 +14,6 @@ import Data.Time (UTCTime)
 import Forgejo.Types.Issue (Issue)
 import GHC.Generics (Generic)
 
-arpOptions :: Options
-arpOptions = defaultOptions{fieldLabelModifier = camelTo2 '_' . drop 3}
-
-runOptions :: Options
-runOptions = defaultOptions{fieldLabelModifier = camelTo2 '_' . drop 3}
-
 data TrackedTime = TrackedTime
   { created :: UTCTime
   , id :: Int
@@ -29,7 +23,6 @@ data TrackedTime = TrackedTime
   }
   deriving stock (Eq, Generic, Show)
 
--- Manual instance because Forgejo uses "ScheduleID" (capitalised) as the JSON key.
 instance FromJSON TrackedTime where
   parseJSON = withObject "TrackedTime" $ \o ->
     TrackedTime
@@ -52,6 +45,3 @@ instance FromJSON TrackedTimePayload where
       <$> o .: "action"
       <*> o .: "run"
       <*> o .: "prior_status"
-
-instance ToJSON TrackedTimePayload where
-  toJSON = genericToJSON arpOptions
