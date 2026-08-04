8e43df32f5c6d600d01f3490ffaeee662fec88d3
xfeusw <khamrakulovkamron@gmail.com>
2026-08-04 14:16:33 +0500

    chore: defaultCreatePullRequestOption

--- src/Forgejo/Types/CreatePullRequestOption.hs
+++ src/Forgejo/Types/CreatePullRequestOption.hs
@@ -1,4 +1,4 @@
-module Forgejo.Types.CreatePullRequestOption (CreatePullRequestOption (..)) where
+module Forgejo.Types.CreatePullRequestOption (CreatePullRequestOption (..), defaultCreatePullRequestOption) where
 
 import Data.Aeson (ToJSON (..), genericToJSON)
 import Data.Aeson.Types (Options (..), camelTo2, defaultOptions)
@@ -7,15 +7,15 @@ import Data.Time (UTCTime)
 import GHC.Generics (Generic)
 
 data CreatePullRequestOption = CreatePullRequestOption
-  { cproAssignee :: Maybe Text
-  , cproAssignees :: Maybe [Text]
+  { cproTitle :: Text
+  , cproHead :: Text
   , cproBase :: Text
+  , cproAssignee :: Maybe Text
+  , cproAssignees :: Maybe [Text]
   , cproBody :: Maybe Text
   , cproDueDate :: Maybe UTCTime
-  , cproHead :: Text
   , cproLabels :: Maybe [Int]
   , cproMilestone :: Maybe Int
-  , cproTitle :: Text
   }
   deriving stock (Eq, Generic, Show)
 
@@ -23,3 +23,6 @@ instance ToJSON CreatePullRequestOption where
   toJSON = genericToJSON (camelToSnake){omitNothingFields = True}
    where
     camelToSnake = defaultOptions{fieldLabelModifier = camelTo2 '_' . drop 4}
+
+defaultCreatePullRequestOption :: Text -> Text -> Text -> CreatePullRequestOption
+defaultCreatePullRequestOption title hd base = CreatePullRequestOption title hd base Nothing Nothing Nothing Nothing Nothing Nothing
