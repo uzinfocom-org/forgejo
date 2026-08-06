a988e55a8dcc6a0495a65ddbf39aa4b5ba8bcd9b
xfeusw <khamrakulovkamron@gmail.com>
2026-08-06 17:49:43 +0500

    chore: added commits to issue

--- src/Forgejo/Types/Issue.hs
+++ src/Forgejo/Types/Issue.hs
@@ -4,7 +4,7 @@ module Forgejo.Types.Issue
   , IssueRepository (..)
   ) where
 
-import Data.Aeson (FromJSON (..), ToJSON (..), Value, genericParseJSON, genericToJSON)
+import Data.Aeson (FromJSON (..), ToJSON (..), genericParseJSON, genericToJSON)
 import Data.Aeson.Types (Options (..), camelTo2, defaultOptions)
 import Data.Text (Text)
 import Data.Time (UTCTime)
@@ -24,7 +24,7 @@ issuePROptions = defaultOptions{fieldLabelModifier = camelTo2 '_' . drop 7}
 issueRepoOptions :: Options
 issueRepoOptions = defaultOptions{fieldLabelModifier = camelTo2 '_' . drop 9}
 
--- Minimal PR reference embedded inside an Issue object
+-- | Minimal PR reference embedded inside an Issue object
 data IssuePRRef = IssuePRRef
   { issuePRMerged :: Bool
   , issuePRMergedAt :: Maybe UTCTime
@@ -39,7 +39,7 @@ instance FromJSON IssuePRRef where
 instance ToJSON IssuePRRef where
   toJSON = genericToJSON issuePROptions
 
--- Minimal repository reference embedded inside an Issue object
+-- | Minimal repository reference embedded inside an Issue object
 data IssueRepository = IssueRepository
   { issueRepoId :: RepoId
   , issueRepoName :: Text
@@ -57,30 +57,55 @@ instance ToJSON IssueRepository where
 -- | This type is used for the body of issues from Forgejo
 data Issue = Issue
   { issueId :: IssueId
+  -- ^ id of 'Issue'
   , issueUrl :: Text
+  -- ^ REST API url of 'Issue'
   , issueHtmlUrl :: Text
+  -- ^ Clickable HTML URL of 'Issue'
   , issueNumber :: Int
+  -- ^ Number of 'Issue' in 'Repository'
   , issueUser :: User
+  -- ^ Author/user ('User') of 'Issue'
   , issueOriginalAuthor :: Text
+  -- ^ Original author of 'Issue' if it's changed
   , issueOriginalAuthorId :: UserId
+  -- ^ Id of original author of 'Issue' if it's changed
   , issueTitle :: Text
+  -- ^ Title of 'Issue'
   , issueBody :: Text
+  -- ^ Body of 'Issue'
   , issueRef :: Text
+  -- ^ References of 'Issue'
   , issueAssets :: [Attachment]
+  -- ^ Assets of 'Issue'
   , issueLabels :: [Label]
+  -- ^ Labels of 'Issue'
   , issueMilestone :: Maybe Milestone
+  -- ^ Milestone of 'Issue'
   , issueAssignee :: Maybe User
+  -- ^ Assignee of 'Issue'
   , issueAssignees :: [User]
+  -- ^ Assignees of 'Issue'
   , issueState :: Text
+  -- ^ State of 'Issue'
   , issueIsLocked :: Bool
+  -- ^ Information about lock of 'Issue'
   , issueComments :: Int
+  -- ^ Amount of comments of 'Issue'
   , issueCreatedAt :: UTCTime
+  -- ^ Created time of 'Issue'
   , issueUpdatedAt :: UTCTime
+  -- ^ Last updated time of 'Issue'
   , issueClosedAt :: Maybe UTCTime
+  -- ^ Closed time of 'Issue'
   , issueDueDate :: Maybe UTCTime
+  -- ^ Deadline of 'Issue'
   , issuePullRequest :: Maybe IssuePRRef
+  -- ^ 'PullRequest' reference of 'Issue'
   , issueRepository :: IssueRepository
+  -- ^ 'Repository' of 'Issue'
   , issuePinOrder :: Int
+  -- ^ Pin order of 'Issue'
   }
   deriving stock (Eq, Generic, Show)
 
